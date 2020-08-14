210dc74f41ffcfc3aad0a266bd8e9e0ad1d51fc7
Serra Saracoglu <62646936+SerraSrcgl@users.noreply.github.com>
2020-08-14 23:50:23 +0200

    Update Sol3_8.java

--- IntroductionToJavaProgramming_/Chapter3/Sol3_8.java
+++ IntroductionToJavaProgramming_/Chapter3/Sol3_8.java
@@ -1,5 +1,37 @@
 public
 class Sol3_8 { public static void main(String [] args){
+Scanner scan=new Scanner(System.in);
+  System.out.println("Please enter three number");
+  int a=scan.nextInt();
+  int b=scan.nextInt();
+  int c=scan.nextInt();
+]
+		int temp;
+		if (b < a || c < a)
+		{
+			if (b < a)
+			{
+				temp = a;
+				a = b;
+				b = temp; 
+			}
+			if (c < a)
+			{
+				temp = a;
+				a = c;
+				c = temp;
+			}
+		}
+		if (c < b)
+		{
+			temp = b;
+			b = c;
+			c = temp;
+		}
 
+		System.out.println(a + " " + b + " " + c);
+	}
 }
-}
+  
+  
+ 
